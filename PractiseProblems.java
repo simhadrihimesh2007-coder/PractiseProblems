6ebf4de522953613321d726e87f2f7238ade3895
simhadrihimesh2007-coder <simhadrihimesh2007@gmail.com>
2026-04-13 14:54:22 +0530

    third commit

--- PractiseProblems.java
+++ PractiseProblems.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class LargestNumberCheck {
+public class FirstSmallest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -13,9 +13,11 @@ public class LargestNumberCheck {
         System.out.print("Enter third number: ");
         int number3 = sc.nextInt();
 
-        System.out.println("Is the first number the largest? " + (number1 > number2 && number1 > number3));
-        System.out.println("Is the second number the largest? " + (number2 > number1 && number2 > number3));
-        System.out.println("Is the third number the largest? " + (number3 > number1 && number3 > number2));
+        if (number1 < number2 && number1 < number3) {
+            System.out.println("Is the first number the smallest? Yes");
+        } else {
+            System.out.println("Is the first number the smallest? No");
+        }
 
         sc.close();
     }
